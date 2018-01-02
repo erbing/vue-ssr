b946b6306dfc951d40af4482d2216374269698f1
zigzga <447587073@qq.com>
2018-01-02 23:30:10 +0800

    add ssr-client and ssr-client

---
+++ components/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="test">
+        this is test components
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'testComponts'
+}
+</script>
+
+<style lang="less">
+    .test{
+        font-size: 30px;
+        font-weight: bold;
+    }
+</style>
+
